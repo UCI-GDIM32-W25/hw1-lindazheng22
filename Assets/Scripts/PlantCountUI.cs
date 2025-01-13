895affd266d2a89936f4227f3f355667628055ca
Linda Zheng <linda.zsy22@gmail.com>
2025-01-12 22:30:13 -0800

    finished everything up to ui

--- Assets/Scripts/PlantCountUI.cs
+++ Assets/Scripts/PlantCountUI.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEditor;
 
 public class PlantCountUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text _plantedText;
     [SerializeField] private TMP_Text _remainingText;
+    //public TextMeshProUGUI _plantedText;
+    //public TextMeshProUGUI _remainingText;
 
     public void UpdateSeeds (int seedsLeft, int seedsPlanted)
     {
-        
+        _plantedText.SetText(seedsPlanted.ToString());
+        _remainingText.SetText(seedsLeft.ToString());
+        //_plantedText.text = seedsPlanted.ToString();
+        //_remainingText.text = seedsLeft.ToString();
+        Debug.Log("Seeds Updated");
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,27 +2,57 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject bunny;
+
     [SerializeField] private float _speed = 1.0f;
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private GameObject _plantPrefab;
     [SerializeField] private int _numSeeds = 5; 
     [SerializeField] private PlantCountUI _plantCountUI;
 
-    private int _numSeedsLeft;
-    private int _numSeedsPlanted;
+    private int _numSeedsLeft = 5;
+    private int _numSeedsPlanted = 0;
 
     private void Start ()
     {
-        
+        _playerTransform = this.transform;
     }
 
     private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W)) //player moves up with W key
+        {
+            _playerTransform.Translate(Vector3.up * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A)) //player moves left with A key
+        {
+            _playerTransform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S)) //player moves down with S key
+        {
+            _playerTransform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D)) //player moves right with D key
+        {
+            _playerTransform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) //seed appears with space key
+        {
+            PlantSeed();
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        if (_numSeeds > 0)
+        {
+            Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+            Debug.Log("number seeds decrease");
+            _numSeeds -= 1;
+            _numSeedsLeft -= 1;
+            _numSeedsPlanted += 1;
+            _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+            
+        }
     }
 }
